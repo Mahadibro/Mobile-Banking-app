cbc2a550e2544b1d1199a2bb10c70fc0244cacca
Aiub Hossaian Mahedi <mahadi131053@gmail.com.com>
2025-12-31 14:38:06 +0600

    Update login logic

--- script/login.js
+++ script/login.js
@@ -1,1 +1,6 @@
-console.log("Login button script loaded.");
+//console.log("Login button script loaded.");
+
+
+document.getElementById("login-btn").addEventListener("click", function() {
+    console.log("Login button clicked.");
+});
