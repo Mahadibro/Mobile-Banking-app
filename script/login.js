2fe045b92d65c7041e681032dcdcf67a515a0d6f
Aiub Hossaian Mahedi <mahadi131053@gmail.com.com>
2025-12-31 16:28:06 +0600

    slove login button

--- script/login.js
+++ script/login.js
@@ -3,4 +3,14 @@
 
 document.getElementById("login-btn").addEventListener("click", function(event) {
     event.preventDefault(); // Prevent form submission
-});
+    const accountNumber = document.getElementById("Account-number").value;
+    
+    const pin = document.getElementById("pin").value;
+    console.log(accountNumber, pin);
+    // Here you can add your login logic, e.g., validate credentials, redirect, etc.
+    if(accountNumber.length ===11 && pin.length >=4) {
+        console.log("Login successful");
+    } else {
+        console.log("Invalid account number or pin number");
+    }
+})
