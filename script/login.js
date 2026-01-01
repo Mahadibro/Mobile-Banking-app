764350b5290b812d6f91e13efcc6ca6a29ea134d
Aiub Hossaian Mahedi <mahadi131053@gmail.com.com>
2026-01-01 14:56:06 +0600

    adding add money

--- script/login.js
+++ script/login.js
@@ -7,10 +7,16 @@ document.getElementById("login-btn").addEventListener("click", function(event) {
     
     const pin = document.getElementById("pin").value;
     console.log(accountNumber, pin);
-    // Here you can add your login logic, e.g., validate credentials, redirect, etc.
-    if(accountNumber.length ===11 && pin.length >=4) {
-        console.log("Login successful");
-    } else {
-        console.log("Invalid account number or pin number");
+    const convertedPin = parseInt(pin);
+    if(accountNumber.length===11){
+        if(convertedPin===1234){
+            window.location.href = "./main.html"; // Redirect to dashboard
+        }
+        else{
+            alert("Incorrect PIN");
+        }
     }
-})
+        else{
+            alert("Account number must be 11 digits");
+        }
+    });
